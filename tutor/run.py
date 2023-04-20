073094ae6645496be8cc573beee650b6c0ac4108
francissame <francissamuel005@gmail.com>
2023-04-20 16:29:45 -0400

    initial commit

--- tutor/run.py
+++ tutor/run.py
@@ -1,11 +1,46 @@
-print("hello")
+# print("hello")
 
-x = 2
-y = 5
+# x = 2
+# y = 5
 
-stn = 'boy'
-my_list = [0, 2.5, 'thin']
+# stn = 'boy'
+# my_list = [1,[4, 6], 2.5, 'thin']
+# my_list = [3, [4,6], 2.5, 'thin']
 
-multi = """ hweloo """
-print(my_list)
-hs
+# tupl = (6, 9)
+
+# multi = "hwelio"
+# print(multi[::-1])
+
+# print(multi[-1:-4:-1])
+
+# a = 8 > 7
+# b = bool(a)
+# print(b)
+
+# x = 10
+# y = 2.0
+
+# z = x + y
+# print(z)
+
+
+# x = 2
+# y = float(x)
+# print(float(x))
+
+# x = 5.8
+
+# print(type(x))
+# x = int(x)
+# print(x)
+# print(type(x))
+
+# x = 2.0
+# x = str(x)
+# print(type(x))
+# print(x)
+
+
+# mystr = "yourname"
+# print(mystr[-1:-4:-1])
